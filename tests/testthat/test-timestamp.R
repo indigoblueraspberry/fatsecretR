95fcf1555bb35bd974dfd870f13c0b1a4dcb313e
Tom Wilson <wilsontom89@gmail.com>
2016-01-13 23:20:06 +0000

    Update test-timestamp.R

--- tests/testthat/test-timestamp.R
+++ tests/testthat/test-timestamp.R
@@ -2,7 +2,7 @@ context("oauth_params")
 
 test_that("timestamp",
           {
-          expect_true(is.numeric(TIMESTAMP))
+          expect_true(is.numeric(TIMESTAMP()))
           expect_that(TIMESTAMP(), not(throws_error()))
           }
     )
