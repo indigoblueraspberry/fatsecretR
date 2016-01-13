634d391374c8ccd28cca8a5052e77a672277aefd
wilsontom <wilsontom89@gmail.com>
2016-01-13 23:06:39 +0000

    add simple test

---
+++ tests/testthat/test-timestamp.R
@@ -0,0 +1,8 @@
+context("oauth_params")
+
+test_that("timestamp",
+          {
+          expect_true(is.numeric(TIMESTAMP))
+          expect_that(TIMESTAMP(), not(throws_error())
+          }
+    )
