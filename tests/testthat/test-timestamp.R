18e12075f9cae3d08cb640e3a2a898635b131f36
wilsontom <wilsontom89@gmail.com>
2016-01-13 23:12:37 +0000

    fix bug

--- tests/testthat/test-timestamp.R
+++ tests/testthat/test-timestamp.R
@@ -3,6 +3,6 @@ context("oauth_params")
 test_that("timestamp",
           {
           expect_true(is.numeric(TIMESTAMP))
-          expect_that(TIMESTAMP(), not(throws_error())
+          expect_that(TIMESTAMP(), not(throws_error()))
           }
     )
