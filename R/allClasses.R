447d6d6bd51e7c1fcf8684aeccf1c65781aa0be5
wilsontom <wilsontom89@gmail.com>
2016-12-07 18:11:29 +0000

    class definitions

---
+++ R/allClasses.R
@@ -0,0 +1,50 @@
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+
+setClass(Class = "fatsecret", representation = representation(
+        ConsumerKey = "character",
+        SharedSecret = "character",
+        RESTURL = "character",
+        httpMethod = "character",
+        signatureMethod = "character",
+        oauthVersion = "character",
+        timestamp = "character",
+        nonce = "character",
+        oauthSignature = "character",
+        methods = "data.frame"
+        )
+)
+
+
+
+setClass(Class = "fatsecret3L", representation = representation(
+        ConsumerKey = "character",
+        SharedSecret = "character",
+        RequestURL = "character",
+        AccessURL = "character",
+        AuthURL = "character",
+        ReqURL = "character",
+        httpMethod = "character",
+        signatureMethod = "character",
+        oauthVersion = "character",
+        timestamp = "character",
+        nonce = "character",
+        oauth_callback = "character",
+        request_token = "character",
+        request_secret = "character",
+        user_request_url = "character",
+        user_token = "character",
+        user_secret = "character",
+        callback_verifier = "character"
+        )
+)
+
+
