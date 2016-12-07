3cdd6582199add4afb3348033deee4b8957ca287
wilsontom <wilsontom89@gmail.com>
2016-12-07 17:30:15 +0000

    oauth_timestamp method

---
+++ R/TimeStamp-method.R
@@ -0,0 +1,36 @@
+#' oauth_timestamp
+#' @rdname oauth_timestamp
+#'
+#' @description Assign a \code{oauth_timestamp} parameter to the current \code{fatsecret} or \code{fatsecret3L}
+#' object
+#'
+#' @include allGenerics.R
+#' @include allClasses.R
+
+
+setMethod(f = "TimeStamp", signature("fatsecret"),
+          function(object){
+
+            objectName <- as.list(match.call())$object
+
+            object@timestamp <- paste0("oauth_timestamp=", as.integer(as.POSIXct(Sys.time()), "GMT"))
+
+            #assign(eval(paste(text = objectName)), object, envir = .GlobalEnv)
+
+            return(object)
+
+          }
+)
+setMethod(f = "TimeStamp", signature("fatsecret3L"),
+          function(object){
+
+            objectName <- as.list(match.call())$object
+
+            object@timestamp <- paste0("oauth_timestamp=", as.integer(as.POSIXct(Sys.time()), "GMT"))
+
+            #assign(eval(paste(text = objectName)), object, envir = .GlobalEnv)
+
+            return(object)
+
+          }
+)
