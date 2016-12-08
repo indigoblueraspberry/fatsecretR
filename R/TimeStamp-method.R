b4dd1db118394c942281d5fa218d382156c714be
wilsontom <wilsontom89@gmail.com>
2016-12-08 14:19:16 +0000

    edit docs

--- R/TimeStamp-method.R
+++ R/TimeStamp-method.R
@@ -21,6 +21,17 @@ setMethod(f = "TimeStamp", signature("fatsecret"),
 
           }
 )
+
+#' oauth_timestamp
+#' @rdname oauth_timestamp
+#'
+#' @description Assign a \code{oauth_timestamp} parameter to the current \code{fatsecret} or \code{fatsecret3L}
+#' object
+#'
+#' @include allGenerics.R
+#' @include allClasses.R
+
+
 setMethod(f = "TimeStamp", signature("fatsecret3L"),
           function(object){
 
