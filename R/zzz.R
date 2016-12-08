bd304ee88f4d90157982095e9dea32daf884ca60
wilsontom <wilsontom89@gmail.com>
2016-12-08 11:34:45 +0000

    fix onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -12,22 +12,19 @@
 
 
 
-.onLoad <- function()
+.onLoad <- function(libname = find.package("fatsecretR"), pkgname = "fatsecretR")
 {
   object <- new("fatsecret")
 
-  object@RequestURL <- "http://www.fatsecret.com/oauth/request_token"
-  object@AccessURL <- "http://www.fatsecret.com/oauth/access_token"
-  object@AuthURL <- "http://www.fatsecret.com/oauth/authorize"
   object@RESTURL <- "http://platform.fatsecret.com/rest/server.api"
   object@httpMethod <- "GET"
-  object@RESTURLenc <- paste0(object@httpMethod, "&",
-                                 URLencode(object@RESTURL, reserved = TRUE))
 
   object@signatureMethod <- paste0("oauth_signature_method=HMAC-SHA1")
   object@oauthVersion <- paste0("oauth_version=1.0")
 
-  object@methods <- readRDS("~/Desktop/methods.RDS")
+  method_file <- system.file("methods.RDS", package = "fatsecretR")
+
+  object@methods <- readRDS(method_file)
 
   assign(eval(paste(text = "fatsecret")), object, envir = .GlobalEnv)
 
