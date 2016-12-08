86787b1bce932df1d1944ce1714421ba9be4e94d
wilsontom <wilsontom89@gmail.com>
2016-12-08 14:59:02 +0000

    duplicate for dual object signature

--- R/APIkeys-method.R
+++ R/APIkeys-method.R
@@ -6,8 +6,6 @@
 #' @include allGenerics.R
 #' @include allClasses.R
 
-
-
 setMethod(f = "APIkeys", signature = "fatsecret",
           function(object, ConsumerKey, SharedSecret){
 
@@ -30,3 +28,35 @@ setMethod(f = "APIkeys", signature = "fatsecret",
 
           }
 )
+
+#' Add API Keys
+#' @rdname apiKeys
+#'
+#' @description Add your private API keys to the \code{fatsecret3L} object
+#'
+#' @include allGenerics.R
+#' @include allClasses.R
+
+setMethod(f = "APIkeys", signature = "fatsecret3L",
+          function(object, ConsumerKey, SharedSecret){
+
+            objectName <- as.list(match.call())$object
+
+            if(!is.character(ConsumerKey)){
+              stop("...API ConsumerKey must be a character")
+            }
+            if(!is.character(SharedSecret)){
+              stop("...API SharedSecret must be a character")
+            }
+            object@ConsumerKey <- ConsumerKey
+            object@SharedSecret <- SharedSecret
+
+            object@oauthConsumer <- paste0("oauth_consumer_key=", ConsumerKey)
+
+            assign(eval(paste(text = objectName)), object, envir = .GlobalEnv)
+
+            return(invisible(NULL))
+
+          }
+)
+
