570f55d8f3c77ccd3dd62f929f8d6fa95b6cfea9
wilsontom <wilsontom89@gmail.com>
2016-12-09 16:33:02 +0000

    add some unit test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(fatsecretR)
+
+test_check("fatsecretR")
---
+++ tests/testthat/test-API.R
@@ -0,0 +1,43 @@
+context("API")
+
+test_that("API",
+          {
+          load("tokens.rda")
+          expect_true(isS4(fatsecret))
+
+          APIkeys(fatsecret, token, secret)
+
+          # test main API methods
+
+          expect_true(is.data.frame(fatsecretR(fatsecret, "getFood", "beer")))
+          expect_true(is.data.frame(fatsecretR(fatsecret, "getFoodID", "1265")))
+
+
+          expect_error(fatsecretR(fatsecret, "getFood", "bber"))
+          expect_error(fatsecretR(fatsecret, "getFoodID", "0000"))
+
+
+          # test internals
+          timeob <- TimeStamp(fatsecret)
+          expect_true(is.character(fatsecret@timestamp))
+
+          nonceob <- OauthNonce(fatsecret)
+          expect_true(is.character(fatsecret@nonce))
+
+
+          # test 3-Legged Authentication
+
+          newAuth("SteveFrench")
+          expect_true(isS4(SteveFrench))
+          APIkeys(SteveFrench, token, secret)
+
+          SteveFrench <- RequestAuthorisation(SteveFrench, params = "oob")
+
+          expect_true(is.character(SteveFrench@request_token))
+          expect_true(is.character(SteveFrench@request_secret))
+          expect_true(is.character(SteveFrench@user_request_url))
+
+
+          }
+)
+
