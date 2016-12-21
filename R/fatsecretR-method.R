749a88b4ac3b30cd2be022601396dd6b8fb0c2e8
wilsontom <wilsontom89@gmail.com>
2016-12-21 14:00:46 +0000

    clean up and fully tested

--- R/fatsecretR-method.R
+++ R/fatsecretR-method.R
@@ -7,7 +7,7 @@
 #' @include allClasses.R
 
 setMethod(f = "fatsecretR", signature = "fatsecret",
-          function(object, method, params){
+          function(object, method, param, user_token = NULL, user_secret = NULL){
 
             methodName <- as.list(match.call())$method
             objectName <- as.list(match.call())$object
@@ -19,54 +19,57 @@ setMethod(f = "fatsecretR", signature = "fatsecret",
 
             RESTmethod <- paste0("method=", object@methods[["method"]][methodInd])
 
-            params_esc <- curlEscape(URLencode(params, reserved = TRUE))
 
-            oauth_param_sign <- paste0(object@methods[["oauth_param"]][methodInd], "%3D", params_esc)
+            if(object@methods[["oauth_param"]][methodInd] == "date"){
+              param <- paste0("date=", as.numeric(as.Date(param)))
+            }else{
+              param <- paste0(object@methods[["oauth_param"]][methodInd], "=", param)
 
-            oauth_param_holder <- object@methods[["oauth_param"]][methodInd]
+            }
 
-            # query string list
+            if(!is.null(user_token)){
+
+              user_token <- paste0("oauth_token=", user_token)
+
+            }
+
+
 
             query_list <- c(RESTmethod, object@oauthConsumer,
                             object@nonce, object@signatureMethod,
                             object@timestamp, object@oauthVersion,
-                            oauth_param_holder)
+                            param, user_token)
 
 
             query_string <- URLencode(as.character(paste(sort(query_list), collapse = "&")), reserved = TRUE)
+            query_string <- gsub("%2B", "%2520", query_string)
 
-
-            query_string <- gsub(oauth_param_holder, oauth_param_sign, query_string)
-
-            #base_string <- paste0(object@RESTURLenc, "&", query_string)
             base_string <- paste0(URLencode(object@RESTURL, reserved = TRUE), "&", query_string)
             base_string <- paste0(object@httpMethod, "&", base_string)
 
 
-            signature_value <- AuthSignature(object, base_string)
-            oauth_param <- paste0(object@methods[["oauth_param"]][methodInd], "%3D", URLencode(params, reserved = TRUE))
 
-            base_string <- gsub(oauth_param_sign, oauth_param, base_string)
+            if(object@methods[["signature"]][methodInd] == 1){
+              signature_value <- AuthSignature(object, base_string)
+            }
+
+            if(object@methods[["signature"]][methodInd] == 2){
+              signature_value <- AuthSignature(object, base_string, params = user_secret)
+            }
 
             pattern_repl <- paste0(object@httpMethod, "&", URLencode(object@RESTURL, reserved = TRUE),"&")
 
             baseURL <- gsub(pattern_repl, "", base_string)
-            #baseURL <- gsub(paste0(object@RESTURLenc,"&"), "", base_string)
-
-            baseURL <- gsub(paste0("%26", oauth_param),"", baseURL)
-            baseURL <- gsub(paste0(oauth_param,"%26"),"", baseURL)
-
-            param_final <- paste0(oauth_param_holder, "=", strsplit(oauth_param, "%3D")[[1]][2])
 
             requestURL <- paste0(
-              object@RESTURL, "?", param_final, "&", URLdecode(baseURL), "&", signature_value)
+              object@RESTURL, "?",URLdecode(baseURL), "&", signature_value)
 
             URLresult <- getURLContent(requestURL)
 
             URLclean <- xmlParser(URLresult, method = methodName)
 
-            return(URLclean)
-            #return(URLresult)
+          return(URLclean)
+
           }
 
 )
